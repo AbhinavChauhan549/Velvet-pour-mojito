74189befdb7af23298bfa1de494086aead064c0b
Abhinav Chauhan <officialabhi549@gmail.com>
2025-07-09 21:07:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
+
+// both of these need to be registered
+// splittext allows to break text into individual text or characters for animation
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+	return (
+		<main>
+			<Navbar />
+			<Hero />
+			<Cocktails />
+			<About />
+			<Art />
+			<Menu />
+			<Contact />
+		</main>
+	)
+}
+
+export default App
